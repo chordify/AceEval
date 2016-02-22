489baf9bd4d9063e96c57faf168beb85ac8b9553
hvkoops <hvkoops@gmail.com>
2016-02-22 21:35:17 +0100

    csv header updates

--- results/boxplotfusion_med.py
+++ results/boxplotfusion_med.py
@@ -42,15 +42,15 @@ mmmvdiff  = np.median(mmpd01['MVOTE'])  - mmaxmed
 mmfdiff 	= np.median(mmpd01['FUSION']) - mmaxmed
 mmalldiff = np.array([mmrdiff, mmmvdiff, mmfdiff])
 
-# s12       = path + "Billboard20" + year + "_Sevth.csv"
-# spd01     = pd.read_csv(s12, sep='\t', header=0, dtype=np.float64)
-# slabels   = spd01.columns.values
-# steams    = slabels[1:-4]
-# smaxmed   = spd01[mmteams].median().max()
-# srdiff    = np.median(spd01['RANDOM']) - smaxmed
-# svdiff    = np.median(spd01['MVOTE'])  - smaxmed
-# sfdiff    = np.median(spd01['FUSION']) - smaxmed
-# salldiff  = np.array([srdiff, svdiff, sfdiff])
+s12       = path + "Billboard20" + year + "_Sevth.csv"
+spd01     = pd.read_csv(s12, sep='\t', header=0, dtype=np.float64)
+slabels   = spd01.columns.values
+steams    = slabels[1:-4]
+smaxmed   = spd01[mmteams].median().max()
+srdiff    = np.median(spd01['RANDOM']) - smaxmed
+svdiff    = np.median(spd01['MVOTE'])  - smaxmed
+sfdiff    = np.median(spd01['FUSION']) - smaxmed
+salldiff  = np.array([srdiff, svdiff, sfdiff])
 
 # statistics
 
@@ -69,27 +69,26 @@ offset 		= (1. / len(ball)) - 0.05
 inx 		  = np.arange(0,len(ball))-offset
 rbarp 		= ax.bar(inx, ralldiff, color=colors[0], width=offset, align='center')
 mmbarp 		= ax.bar(inx+offset, mmalldiff, color=colors[2], width=offset, align='center')
-# sbarp 		= ax.bar(inx+offset+offset, salldiff, color=colors[3], width=offset, align='center')
+sbarp 		= ax.bar(inx+offset+offset, salldiff, color=colors[3], width=offset, align='center')
 barlabels = ['$\\textsc{rand}$', '$\\textsc{mv}$', '$\\textsc{fusion}$']
 ax.set_xticklabels(barlabels, rotation='vertical')
 
 starfont = {'family' : 'serif', 'weight' : 'bold', 'size'   : 10}
 ax.text(rbarp[2].get_x() + rbarp[2].get_width()/2., 1.05*rbarp[2].get_height(), stars(stats.wilcoxon(rpd01['FUSION'],rpd01['MVOTE'])[1]), ha='center', va='bottom', **starfont)
 ax.text(mmbarp[2].get_x() + mmbarp[2].get_width()/2., 1.05*mmbarp[2].get_height(), stars(stats.wilcoxon(mmpd01['FUSION'],mmpd01['MVOTE'])[1]), ha='center', va='bottom', **starfont)
-# ax.text(sbarp[2].get_x() + sbarp[2].get_width()/2., 1.05*sbarp[2].get_height(), stars(stats.wilcoxon(spd01['FUSION'],spd01['MVOTE'])[1]), ha='center', va='bottom', **starfont)
+ax.text(sbarp[2].get_x() + sbarp[2].get_width()/2., 1.05*sbarp[2].get_height(), stars(stats.wilcoxon(spd01['FUSION'],spd01['MVOTE'])[1]), ha='center', va='bottom', **starfont)
 
-# allvals = np.concatenate([mmalldiff, ralldiff, salldiff])
-allvals = np.concatenate([mmalldiff, ralldiff])
+allvals = np.concatenate([mmalldiff, ralldiff, salldiff])
 # plt.yticks(np.arange(-6,-6),fontsize=16)
 fontsize = 13
 ax.tick_params(labelsize=fontsize)
-# plt.ylim([np.floor(np.min(allvals)),np.ceil(np.max(allvals))])
+plt.ylim([np.floor(np.min(allvals)),np.ceil(np.max(allvals))])
 plt.ylim([-15,8])
 plt.yticks(np.arange(-13,8,1))
 
 plt.ylabel("\% Chord Symbol Recall difference",fontsize=fontsize)
 plt.xticks(np.arange(0,len(ball),1),fontsize=fontsize)
-# ax.legend((rbarp[0], mmbarp[0], sbarp[0]), (ball), loc=4,fontsize=fontsize, frameon=True)
+ax.legend((rbarp[0], mmbarp[0], sbarp[0]), (ball), loc=4,fontsize=fontsize, frameon=True)
 # ax.yaxis.grid(False)
 
 sns.despine(left=True)
