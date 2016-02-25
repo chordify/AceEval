18f88cceb3813ff5a6841cc8599c3841d1bbc7ab
Hendrik Vincent Koops <hvkoops@gmail.com>
2016-02-25 15:28:22 +0100

    plotting correlation update with pearsons r

--- results/correlation_both.py
+++ results/correlation_both.py
@@ -24,7 +24,7 @@ ral 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboar
 ralc 	= ral.columns.values
 rsa 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboard20' + year + '_ROOT_SA.csv', sep='\t')
 rinx 	= rsa.columns.values[0:-2]
-rtt		= stats.ttest_ind(np.asarray(ral[ralc[1:-4]]).reshape(-1), np.asarray(rsa[rinx]).reshape(-1))
+rtt		= stats.spearmanr(np.asarray(ral[ralc[1:-4]]).reshape(-1), np.asarray(rsa[rinx]).reshape(-1))
 print "root ttest =" + str(rtt)
 ax1.scatter(ral[ralc[1:-4]]/100, rsa[rinx], color=colors[2], s=dotsize)
 
@@ -32,7 +32,7 @@ mmal 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboa
 mmalc 	= mmal.columns.values
 mmsa 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboard20' + year + '_MajMin_SA.csv', sep='\t')
 mminx 	= mmsa.columns.values[0:-2]
-mmtt	= stats.ttest_ind(np.asarray(mmal[mmalc[1:-4]]).reshape(-1), np.asarray(mmsa[mminx]).reshape(-1))
+mmtt	= stats.spearmanr(np.asarray(mmal[mmalc[1:-4]]).reshape(-1), np.asarray(mmsa[mminx]).reshape(-1))
 print "mm ttest =" + str(mmtt)
 ax1.scatter(mmal[mmalc[1:-4]]/100, mmsa[mminx], color=colors[1], s=dotsize)
 
@@ -40,7 +40,7 @@ sal 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboar
 salc 	= sal.columns.values
 ssa 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboard20' + year + '_Sevth_SA.csv', sep='\t')
 sinx 	= ssa.columns.values[0:-2]
-stt		= stats.ttest_ind(np.asarray(sal[salc[1:-4]]).reshape(-1), np.asarray(ssa[sinx]).reshape(-1))
+stt		= stats.spearmanr(np.asarray(sal[salc[1:-4]]).reshape(-1), np.asarray(ssa[sinx]).reshape(-1))
 print "mm ttest =" + str(stt)
 ax1.scatter(sal[salc[1:-4]]/100, ssa[sinx], color=colors[0], s=dotsize)
 
@@ -50,7 +50,7 @@ ral 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboar
 ralc 	= ral.columns.values
 rsa 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboard20' + year + '_ROOT_SA.csv', sep='\t')
 rinx 	= rsa.columns.values[0:-2]
-rtt		= stats.ttest_ind(np.asarray(ral[ralc[1:-4]]).reshape(-1), np.asarray(rsa[rinx]).reshape(-1))
+rtt		= stats.spearmanr(np.asarray(ral[ralc[1:-4]]).reshape(-1), np.asarray(rsa[rinx]).reshape(-1))
 print "root ttest =" + str(rtt)
 ax2.scatter(ral[ralc[1:-4]]/100, rsa[rinx], color=colors[2], s=dotsize)
 
@@ -58,7 +58,7 @@ mmal 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboa
 mmalc 	= mmal.columns.values
 mmsa 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboard20' + year + '_MajMin_SA.csv', sep='\t')
 mminx 	= mmsa.columns.values[0:-2]
-mmtt	= stats.ttest_ind(np.asarray(mmal[mmalc[1:-4]]).reshape(-1), np.asarray(mmsa[mminx]).reshape(-1))
+mmtt	= stats.spearmanr(np.asarray(mmal[mmalc[1:-4]]).reshape(-1), np.asarray(mmsa[mminx]).reshape(-1))
 print "mm ttest =" + str(mmtt)
 ax2.scatter(mmal[mmalc[1:-4]]/100, mmsa[mminx], color=colors[1], s=dotsize)
 
@@ -66,7 +66,7 @@ sal 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboar
 salc 	= sal.columns.values
 ssa 	= pd.read_csv('/Users/hvkoops/repos/aceeval/results/bb' + year + '/Billboard20' + year + '_Sevth_SA.csv', sep='\t')
 sinx 	= ssa.columns.values[0:-2]
-stt		= stats.ttest_ind(np.asarray(sal[salc[1:-4]]).reshape(-1), np.asarray(ssa[sinx]).reshape(-1))
+stt		= stats.spearmanr(np.asarray(sal[salc[1:-4]]).reshape(-1), np.asarray(ssa[sinx]).reshape(-1))
 print "mm ttest =" + str(stt)
 ax2.scatter(sal[salc[1:-4]]/100, ssa[sinx], color=colors[0], s=dotsize)
 
